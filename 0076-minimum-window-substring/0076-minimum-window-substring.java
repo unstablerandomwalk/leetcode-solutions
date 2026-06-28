0952ae79638a45e49942fa5cf013fbfebea8da91
Hardik Khaneja <khanejahardik48@gmail.com>
2026-06-28 20:07:35 +0530

    Time: 17 ms (51.22%), Space: 47.5 MB (34.21%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,42 +1,46 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (t.isEmpty() || s.isEmpty()) return "";
-
-        Map<Character, Integer> need = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            need.merge(c, 1, Integer::sum);
+        if (s.length() < t.length()) {
+            return "";
         }
 
-        int required = need.size(); 
-        int have = 0;
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char ch : t.toCharArray()) {
+            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+        }
 
-        Map<Character, Integer> window = new HashMap<>();
-        int start = 0;
-        int minLength = Integer.MAX_VALUE;
-        int resStart = 0;
-        for (int end = 0; end < s.length(); end++) {
-            char c = s.charAt(end);
-            window.merge(c, 1, Integer::sum);
+        int targetCharsRemaining = t.length();
+        int[] minWindow = {0, Integer.MAX_VALUE};
+        int startIndex = 0;
 
-            if (need.containsKey(c) && window.get(c).intValue() == need.get(c).intValue()) {
-                have++;
+        for (int endIndex = 0; endIndex < s.length(); endIndex++) {
+            char ch = s.charAt(endIndex);
+            if (charCount.containsKey(ch) && charCount.get(ch) > 0) {
+                targetCharsRemaining--;
             }
+            charCount.put(ch, charCount.getOrDefault(ch, 0) - 1);
 
-            while (have == required) {
-                if (end - start + 1 < minLength) {
-                    minLength = end - start + 1;
-                    resStart = start;
+            if (targetCharsRemaining == 0) {
+                while (true) {
+                    char charAtStart = s.charAt(startIndex);
+                    if (charCount.containsKey(charAtStart) && charCount.get(charAtStart) == 0) {
+                        break;
+                    }
+                    charCount.put(charAtStart, charCount.getOrDefault(charAtStart, 0) + 1);
+                    startIndex++;
                 }
 
-                char left = s.charAt(start);
-                window.merge(left, -1, Integer::sum);
-                if (need.containsKey(left) && window.get(left) < need.get(left)) {
-                    have--;
+                if (endIndex - startIndex < minWindow[1] - minWindow[0]) {
+                    minWindow[0] = startIndex;
+                    minWindow[1] = endIndex;
                 }
-                start++;
+
+                charCount.put(s.charAt(startIndex), charCount.getOrDefault(s.charAt(startIndex), 0) + 1);
+                targetCharsRemaining++;
+                startIndex++;
             }
         }
 
-        return minLength == Integer.MAX_VALUE ? "" : s.substring(resStart, resStart + minLength);
+        return minWindow[1] >= s.length() ? "" : s.substring(minWindow[0], minWindow[1] + 1);        
     }
 }

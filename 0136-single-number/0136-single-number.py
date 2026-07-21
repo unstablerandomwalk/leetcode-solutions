283745ffdef6fb28e74318d3958b93ce0936ab23
Hardik Khaneja <khanejahardik48@gmail.com>
2026-07-22 03:19:29 +0530

    Time: 7 ms (29.96%), Space: 21.5 MB (22.72%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -3,7 +3,7 @@ class Solution:
         d = {}
         for i in nums:
             if i in d:
-                d[i] = 2
+                d[i] += 1
             else:
                 d[i] = 1
         for a,b in d.items():

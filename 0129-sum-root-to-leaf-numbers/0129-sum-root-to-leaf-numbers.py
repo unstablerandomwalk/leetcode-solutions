072d676ab4b3b9ce6e2226410e9b1097f01321e1
Hardik Khaneja <khanejahardik48@gmail.com>
2026-07-25 23:17:03 +0530

    Time: 0 ms (100%), Space: 19.2 MB (93.65%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -8,10 +8,14 @@ class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         total = 0
         def helper(node, create_num):
+            nonlocal total
             if not node:
-                return 0
+                return
             create_num = (create_num * 10) + node.val
             if not node.left and not node.right:
-                return create_num
-            return helper(node.left, create_num) + helper(node.right, create_num)
-        return helper(root,0)
+                total += create_num
+                return
+            helper(node.left, create_num)
+            helper(node.right, create_num)
+        helper(root, 0)
+        return total

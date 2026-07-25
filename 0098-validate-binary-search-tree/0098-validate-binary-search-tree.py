95b547f172e0beae0606e820f6195d1dd5355c1b
Hardik Khaneja <khanejahardik48@gmail.com>
2026-07-25 22:58:55 +0530

    Time: 3 ms (33.95%), Space: 20.9 MB (73.7%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -5,5 +5,5 @@ class Solution:
                 return True
             if node.val <= lo or node.val >= hi:
                 return False
-            return check(node.left, lo, node.val) and check(node.right, node.val,hi)
+            return check(node.left, lo, node.val) and check(node.right, node.val, hi)
         return check(root, float('-inf'), float('inf'))

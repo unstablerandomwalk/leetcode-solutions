00ef78e252f8ba6cc9753d2de8b073e6a2b51aba
Hardik Khaneja <khanejahardik48@gmail.com>
2026-09-02 16:43:57 +0530

    Time: 209 ms (99.2%), Space: 21.6 MB (72.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,18 +2,18 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        count = 0
-        def sink(r,c):
-            if r<0 or r>=m or c<0 or c>=n or grid[r][c] != "1":
+        def turn(r,s):
+            if r < 0 or r >=m or s<0 or s>=n or grid[r][s] == "0":
                 return
-            grid[r][c] = 0
-            sink(r+1,c)
-            sink(r-1,c)
-            sink(r,c+1)
-            sink(r,c-1)
+            grid[r][s] = "0"
+            turn(r-1,s)
+            turn(r+1,s)
+            turn(r,s-1)
+            turn(r,s+1)
+        count = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1":
-                    sink(i,j)
-                    count += 1
+                    count+=1
+                    turn(i,j)
         return count

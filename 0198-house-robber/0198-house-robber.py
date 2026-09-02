7a93b8717b7795514b142e8f049b312c2e300317
Hardik Khaneja <khanejahardik48@gmail.com>
2026-09-02 17:00:40 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,8 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums)==0:
+        if len(nums) == 0:
             return 0
-        money = [0,nums[0]]
-        for i in range(2,len(nums)+1):
+        money = [0, nums[0]]
+        for i in range(2, len(nums)+1):
             money.append(max(money[i-2] + nums[i-1], money[i-1]))
         return money[-1]

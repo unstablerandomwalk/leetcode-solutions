4f54fe32182513d5f659300b6a9c64b573839206
Hardik Khaneja <khanejahardik48@gmail.com>
2026-09-08 22:06:01 +0530

    Time: 19 ms (88.19%), Space: 53.5 MB (24.7%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+        stack = []
+        while fast and fast.next:
+            stack.append(slow.val)
+            fast = fast.next.next
+            slow=slow.next
+        if fast:
+            slow=slow.next
+        while slow:
+            if stack.pop() != slow.val:
+                return False
+            slow=slow.next
+        return True
